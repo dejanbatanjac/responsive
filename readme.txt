cd87c7869fe986b85a54199593fb51ac7062a8f9
dejanbatanjac <dejan.batanjac@gmail.com>
2016-11-01 21:37:25 +0100

    Comments styles improved.

--- readme.txt
+++ readme.txt
@@ -1,22 +1,22 @@
-=== responsive ===
+=== Responsive ===
 
 Contributors: Dejan Batanjac
 Tags: responsive, scalable, scale, transform, -moz-transform, -webkit-transform
 Requires at least: 3.0
 Tested up to: 4.5 beta
-Stable tag: 1.0
+Stable tag: 1.1.0
 
 Resizes any non responsive web site, and makes it responsive with the simple trick of CSS scaling achieved using jQuery
 
 == Description ==
-This simple WordPress plugin inserts smart jQuery code to your website header and makes old, non-responsive web sites automatically responsive. It doesn't use Bootstrap, nor any responsive grid system. 
+This simple WordPress plugin inserts smart jQuery code to your website header and makes old, non-responsive web sites automatically responsive. It doesn't use Bootstrap, nor any responsive grid system.
 
 The only requirement is JavaScript (jQuery) being enabled in your browser. If no JavaScrpt enabled, then the plugin will not have any impact.
 
 == Installation ==
 
-1. Upload `responsive`  folder to the `/wp-content/plugins/` directory
-1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Upload the plugin
+1. Activate the plugin
 
 
 == Screenshots ==
@@ -26,6 +26,9 @@ The only requirement is JavaScript (jQuery) being enabled in your browser. If no
 
 == Changelog ==
 
-= 1.0 =
 
+= 1.1.0 =
+Tabs into spaces.
+
+= 1.0 =
 Initial version
--- responsive.php
+++ responsive.php
@@ -1,80 +1,72 @@
 <?php
-/*
-Plugin Name: Responsive
-Plugin URI: http://programming-review.com/
-Description: Can make your theme responsive without any effort
-Author: Dejan Batanjac
-Version: 1.0
-Author URI: http://programming-review.com/
-License: GPLv2 or later
-  
-*/
-
+/**
+ * Plugin Name: Responsive
+ * Plugin URI: http://programming-review.com/
+ * Description: Will make your theme responsive without any effort
+ * Author: Dejan Batanjac
+ * Version: 1.1.0
+ * Author URI: https://programming-review.com/
+ * License: GPLv2 or later
+ */
+if ( ! class_exists( 'Responsive' ) ) :
 class Responsive {
 
-	/*--------------------------------------------*
-	 * Constants
-	 *--------------------------------------------*/
-	const name = 'responsive';
-	const slug = 'responsive';
-	
-	/**
-	 * [__construct makes sure jQuery h/b added]
-	 */
-	function __construct() {
+  // Constants
+  const NAME = 'responsive';
+  const SLUG = 'responsive';
 
+  //////////////////////////////////////////////////
+  // Constructor makes sure jQuery will be added. //
+  //////////////////////////////////////////////////
+  function __construct() {
 
-		add_action( 'wp_enqueue_scripts', 'please_add_jquery' );
-		function please_add_jquery() {    wp_enqueue_script( 'jquery' ); }		
-		
+    add_action( 'wp_enqueue_scripts', 'add_jquery' );
+    function add_jquery() {
+      wp_enqueue_script( 'jquery' );
+    }
 
-		//hook to wp_head
-		add_action('wp_head',array( &$this, 'draw_' ));		
+    add_action( 'wp_head', array( &$this, 'draw_' ) );
 
+  } // End constructor().
 
-	}
+  ///////////////////////////////////
+  // Main JavaScript resize script //
+  ///////////////////////////////////
+  function draw_() {
+    ?>
+    <script>
 
-	/**
-	 * [draw_ Scales with factor X]
-	 * @return [bool] [true]
-	 */
-	function draw_(){
-		?>
-		<script>
+      jQuery(window).resize(function() {
+        safescale();
+      });
+      jQuery(document).ready(function() {
+        jQuery(window).trigger('resize');
+        jQuery(window).trigger('resize');
+      });
 
-			jQuery(window).resize(function() {  	
-				safescale();    
-			});
-			jQuery(document).ready(function() { 
-				jQuery(window).trigger('resize');
-				jQuery(window).trigger('resize');
+      function scale(factor_x){
+        jQuery('html').css("transform","scale("+factor_x+","+factor_x+")");
+        jQuery('html').css("-moz-transform","scale("+factor_x+","+factor_x+")");
+        jQuery('html').css("-webkit-transform","scale("+factor_x+","+factor_x+")");
+        jQuery('html').css("-o-transform","scale("+factor_x+","+factor_x+")");
+      }
 
-			});
+      function safescale(){
 
+        factor_x = jQuery(window).width() / jQuery('body').width() ;
 
-			function scale(factor_x){
-				jQuery('html').css("transform","scale("+factor_x+","+factor_x+")");  
-				jQuery('html').css("-moz-transform","scale("+factor_x+","+factor_x+")");       
-				jQuery('html').css("-webkit-transform","scale("+factor_x+","+factor_x+")");
-				jQuery('html').css("-o-transform","scale("+factor_x+","+factor_x+")");
-			}  
+        jQuery("html").css({"position": "absolute", "margin": "0px", "padding": "0px"});
+        jQuery("body").css({"position": "absolute", "margin": "0px", "padding": "0px"});
+        scale(factor_x);
+      }
+    </script>
 
-			function safescale(){
-
-				factor_x = jQuery(window).width() / jQuery('body').width() ;
-
-				jQuery("html").css({"position": "absolute", "margin": "0px", "padding": "0px"});
-				jQuery("body").css({"position": "absolute", "margin": "0px", "padding": "0px"});
-				scale(factor_x);
-			}
-		</script> 
-
-		<?php
-		return true;
-	}
+    <?php
+    return true;
+  }
 
 
 } // end class
-new Responsive();
+endif;
 
-?>
+new Responsive();
